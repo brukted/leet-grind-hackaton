e0f8ca934714d78119d1644289e95f24772c98b1
Bruk Tedla <biruk.tedla93@gmail.com>
2023-04-19 09:43:32 +0300

    extract environment variables

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,11 +7,12 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const protectRoute = require('./middlewares/auth.middleware.js');
 const JSendResponse = require('./utils/jsend-response.js').JSendResponse;
-var cors = require('cors')
+var cors = require('cors');
+const { mongoDbConnectionString } = require('./config.js');
 
 
 // Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/', { dbName: 'projectFinder' })
+mongoose.connect(mongoDbConnectionString, { dbName: 'projectFinder' })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB', err));
 
