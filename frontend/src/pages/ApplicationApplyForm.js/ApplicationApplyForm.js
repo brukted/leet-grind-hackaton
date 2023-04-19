8953d95c62455c9a5eab617981a58aae855c7ae6
aben-bel <abenezer.sbelay@gmail.com>
2023-04-19 18:27:05 +0300

    [Frontend] implement detail-page for apply

--- frontend/src/pages/ApplicationApplyForm.js/ApplicationApplyForm.js
+++ frontend/src/pages/ApplicationApplyForm.js/ApplicationApplyForm.js
@@ -1,57 +1,109 @@
 import React, { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { ideasState } from "../../recoil_state";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import IdeaCard from "../HomePage/components/IdeaCard";
 
 const ApplicationApply = () => {
   const { id } = useParams();
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [ideas, setIdeas] = useRecoilState(ideasState);
   const [idea, setIdea] = useState({});
-  
+  const navigate = useNavigate();
+
   useEffect(() => {
-    const result = ideas.filter((val) => {
-      return val.id == id;
-    });
-    setIdea((result && result[0]) || []);
-    console.log(idea);
-  }, []);
+    const result = ideas.find((val) => val.id == id);
+    setIdea(result || {});
+  }, [ideas, id]);
 
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  };
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handleMessageChange = (event) => {
-    setMessage(event.target.value);
-  };
+  const handleMessageChange = (event) => setMessage(event.target.value);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     // TODO: send form data to server
   };
 
+  const handleGoBack = () => {
+    navigate(`/home`);
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <h2 class="text-2xl font-bold">Apply for "{idea.description}"</h2>
-      <div>
-        <label>Name:</label>
-        <input type="text" value={name} onChange={handleNameChange} />
+    <form class="max-w-2xl mx-auto mt-8" onSubmit={handleSubmit}>
+      <a
+        href="#"
+        class="flex items-center text-gray-500 hover:text-gray-700"
+        onClick={handleGoBack}
+      >
+        <svg
+          class="h-5 w-5 mr-1"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
+        </svg>
+        Back
+      </a>
+      <br></br>
+      <div className="p-4 bg-white rounded-lg shadow-lg">
+        <div className="flex items-center justify-between mb-4">
+          <div className="text-lg font-bold">
+            {idea && idea.author && idea.author.name}
+          </div>
+        </div>
+        <div className="mb-4">{idea.description}</div>
+        <div className="flex flex-wrap gap-2 mb-4">
+          {idea &&
+            idea.tags &&
+            idea.tags.map((tag) => (
+              <button
+                key={tag}
+                id={tag}
+                className="px-2 py-1 mr-2 border rounded-lg"
+              >
+                {tag}
+              </button>
+            ))}
+        </div>
+        <div className="flex items-center justify-between">
+          <a
+            href={idea.githubLink}
+            target="_blank"
+            rel="noreferrer"
+            className="text-blue-500 hover:underline"
+          >
+            View on Github
+          </a>
+        </div>
       </div>
-      <div>
-        <label>Email:</label>
-        <input type="email" value={email} onChange={handleEmailChange} />
+      <br />
+      <div class="pb-4">
+        <label
+          for="message"
+          class="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Message:
+        </label>
+        <textarea
+          id="message"
+          name="message"
+          class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          rows="3"
+          value={message}
+          onChange={handleMessageChange}
+        ></textarea>
       </div>
-      <div>
-        <label>Message:</label>
-        <textarea value={message} onChange={handleMessageChange} />
+      <div class="flex justify-end">
+        <button
+          type="submit"
+          class="inline-flex items-center px-4 py-2 bg-green-500 border border-transparent rounded-md font-semibold text-white tracking-widest hover:bg-green-600 active:bg-green-700 focus:outline-none focus:border-green-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150"
+        >
+          Apply
+        </button>
       </div>
-      <button type="submit">Apply</button>
     </form>
   );
 };
