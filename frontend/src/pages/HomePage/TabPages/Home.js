a41c7ad14accebd918b66778d274687b07eb91b6
aben-bel <abenezer.sbelay@gmail.com>
2023-04-20 00:52:59 +0300

    [Frontend] fix color issue

--- frontend/src/pages/HomePage/TabPages/Home.js
+++ frontend/src/pages/HomePage/TabPages/Home.js
@@ -88,7 +88,7 @@ const Home = () => {
                 {selectedTags.includes(tag) && (
                   <button
                     type="button"
-                    class="flex-shrink-0 ml-1.5 h-4 w-4 rounded-full inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500"
+                    class="flex-shrink-0 ml-1.5 h-4 w-4 rounded-full inline-flex items-center justify-center text-white hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500"
                     onClick={() => handleTagDeselect(tag)}
                   >
                     <span class="sr-only">Remove {tag}</span>
