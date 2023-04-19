7ae435e9ff47c312f4401e58b1a841028ba15be7
aben-bel <abenezer.sbelay@gmail.com>
2023-04-19 14:54:57 +0300

    [Frontend] table for application

--- frontend/src/pages/HomePage/TabPages/Application.js
+++ frontend/src/pages/HomePage/TabPages/Application.js
@@ -3,9 +3,51 @@ import { Clipboard, Clock, CheckCircle, XCircle } from "phosphor-react";
 import TabItem from "../components/TabItem";
 import ApplicationTableRow from "../components/ApplicationTableRow.js";
 
-
 const Applications = () => {
   const [activeTab, setActiveTab] = useState("Pending");
+
+  const [applications, setApplications] = useState([
+    {
+      name: "John Doe",
+      date: "April 17, 2023",
+      status: "Pending",
+    },
+    {
+      name: "Jane Smith",
+      date: "April 15, 2023",
+      status: "Pending",
+    },
+    {
+      name: "Alex Johnson",
+      date: "April 12, 2023",
+      status: "Accepted",
+    },
+    {
+      name: "Sarah Lee",
+      date: "April 10, 2023",
+      status: "Accepted",
+    },
+    {
+      name: "David Kim",
+      date: "April 7, 2023",
+      status: "Accepted",
+    },
+    {
+      name: "Olivia Brown",
+      date: "April 5, 2023",
+      status: "Accepted",
+    },
+    {
+      name: "Emily Wilson",
+      date: "April 2, 2023",
+      status: "Accepted",
+    },
+    {
+      name: "Peter Parker",
+      date: "March 30, 2023",
+      status: "Rejected",
+    },
+  ]);
 
   const handleTabClick = (tabName) => {
     setActiveTab(tabName);
@@ -16,7 +58,7 @@ const Applications = () => {
 
     return (
       <button
-        className={`flex items-center justify-start w-full h-12 px-4 transition-colors duration-200 rounded-none focus:outline-none ${
+        className={`flex items-center justify-center w-full px-4 py-2 text-sm font-medium rounded-none transition-colors duration-200 ${
           isActive
             ? "text-white bg-primary"
             : "text-gray-500 hover:text-white hover:bg-secondary"
@@ -26,10 +68,10 @@ const Applications = () => {
         <Icon
           size={isActive ? 28 : 24}
           weight={isActive ? "bold" : "regular"}
+          className="mr-2"
         />
-        <span className="ml-4 text-sm font-medium">
-          {tabName} {count > 0 && `(${count})`}
-        </span>
+        <span>{tabName}</span>
+        {count > 0 && <span className="ml-2">{`(${count})`}</span>}
       </button>
     );
   };
@@ -39,74 +81,25 @@ const Applications = () => {
       <div className="px-4 pt-8">
         <h2 className="text-xl font-bold">Applications</h2>
       </div>
-      <nav className="flex-1 mt-8 space-y-2">
-        <TabItem
-          tabName="Applications"
-          Icon={Clipboard}
-          onClick={handleTabClick}
-          isActive={activeTab === "Applications"}
-        />
+      <nav className="flex mt-8 border-b border-gray-300">
         <SubNavItem tabName="Pending" Icon={Clock} count={2} />
         <SubNavItem tabName="Accepted" Icon={CheckCircle} count={5} />
         <SubNavItem tabName="Rejected" Icon={XCircle} count={1} />
       </nav>
-      <div className="px-4 py-8">
+      <div className="flex-1 px-4 py-8 overflow-auto">
         {activeTab === "Pending" && (
           <ApplicationTableRow
-            rows={[
-              {
-                name: "John Doe",
-                date: "April 17, 2023",
-                status: "Pending",
-              },
-              {
-                name: "Jane Smith",
-                date: "April 15, 2023",
-                status: "Pending",
-              },
-            ]}
+            rows={applications.filter((val) => val.status === "Pending")}
           />
         )}
         {activeTab === "Accepted" && (
           <ApplicationTableRow
-            rows={[
-              {
-                name: "Alex Johnson",
-                date: "April 12, 2023",
-                status: "Accepted",
-              },
-              {
-                name: "Sarah Lee",
-                date: "April 10, 2023",
-                status: "Accepted",
-              },
-              {
-                name: "David Kim",
-                date: "April 7, 2023",
-                status: "Accepted",
-              },
-              {
-                name: "Olivia Brown",
-                date: "April 5, 2023",
-                status: "Accepted",
-              },
-              {
-                name: "Emily Wilson",
-                date: "April 2, 2023",
-                status: "Accepted",
-              },
-            ]}
+            rows={applications.filter((val) => val.status === "Accepted")}
           />
         )}
         {activeTab === "Rejected" && (
           <ApplicationTableRow
-            rows={[
-              {
-                name: "Peter Parker",
-                date: "March 30, 2023",
-                status: "Rejected",
-              },
-            ]}
+            rows={applications.filter((val) => val.status === "Rejected")}
           />
         )}
       </div>
--- frontend/src/pages/HomePage/components/ApplicationTableRow.js.js
+++ frontend/src/pages/HomePage/components/ApplicationTableRow.js.js
@@ -1,32 +1,66 @@
-import React, { useState } from "react";
+import React from "react";
 import { Clipboard, Clock, CheckCircle, XCircle } from "phosphor-react";
-import TabItem from "./TabItem";
 
-const ApplicationTableRow = ({ name, date, status }) => {
+const ApplicationTableRow = ({ rows }) => {
   return (
-    <tr>
-      <td className="px-6 py-4 whitespace-nowrap">
-        <div className="text-sm font-medium text-gray-900">{name}</div>
-        <div className="text-sm text-gray-500">{date}</div>
-      </td>
-      <td className="px-6 py-4 whitespace-nowrap">
-        {status === "Pending" && (
-          <span className="px-2 inline-flex text-xs font-semibold rounded-full bg-yellow-100 text-yellow-800">
-            {status}
-          </span>
-        )}
-        {status === "Accepted" && (
-          <span className="px-2 inline-flex text-xs font-semibold rounded-full bg-green-100 text-green-800">
-            {status}
-          </span>
-        )}
-        {status === "Rejected" && (
-          <span className="px-2 inline-flex text-xs font-semibold rounded-full bg-red-100 text-red-800">
-            {status}
-          </span>
-        )}
-      </td>
-    </tr>
+    <table className="min-w-full divide-y divide-gray-200">
+      <thead className="bg-gray-50">
+        <tr>
+          <th
+            scope="col"
+            className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+          >
+            Name
+          </th>
+          <th
+            scope="col"
+            className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+          >
+            Date
+          </th>
+          <th
+            scope="col"
+            className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+          >
+            Status
+          </th>
+        </tr>
+      </thead>
+      <tbody className="bg-white divide-y divide-gray-200">
+        {rows.map((row, index) => (
+          <tr key={index}>
+            <td className="px-6 py-4 whitespace-nowrap">
+              <div className="text-sm font-medium text-gray-900">
+                {row.name}
+              </div>
+            </td>
+            <td className="px-6 py-4 whitespace-nowrap">
+              <div className="text-sm font-medium text-gray-900">
+                {row.date}
+              </div>
+            </td>
+
+            <td className="px-6 py-4 whitespace-nowrap">
+              {row.status === "Pending" && (
+                <span className="inline-flex px-2 text-xs font-semibold text-yellow-800 bg-yellow-100 rounded-full">
+                  {row.status}
+                </span>
+              )}
+              {row.status === "Accepted" && (
+                <span className="inline-flex px-2 text-xs font-semibold text-green-800 bg-green-100 rounded-full">
+                  {row.status}
+                </span>
+              )}
+              {row.status === "Rejected" && (
+                <span className="inline-flex px-2 text-xs font-semibold text-red-800 bg-red-100 rounded-full">
+                  {row.status}
+                </span>
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
