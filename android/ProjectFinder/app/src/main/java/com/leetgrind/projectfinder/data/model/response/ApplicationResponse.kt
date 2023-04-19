40ed999d4898a44e2ddd0dbaade51b3bd0f7f813
Nazrawi Demeke Workalemahu <nazrawi.demeke@gmail.com>
2023-04-19 20:59:40 +0300

    fix models

--- android/ProjectFinder/app/src/main/java/com/leetgrind/projectfinder/data/model/response/ApplicationResponse.kt
+++ android/ProjectFinder/app/src/main/java/com/leetgrind/projectfinder/data/model/response/ApplicationResponse.kt
@@ -3,7 +3,9 @@ package com.leetgrind.projectfinder.data.model.response
 import com.google.gson.annotations.SerializedName
 
 data class ApplicationResponse(
+    @SerializedName("gigModel")
     val gig: GigResponse,
+    @SerializedName("applicantModel")
     val applicant: ProfileResponse,
     val note: String,
     val status: ApplicationStatus,
