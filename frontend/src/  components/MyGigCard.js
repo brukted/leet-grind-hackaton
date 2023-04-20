77580125bcaf49bb0204495a35e62c2120c08da3
Abenezer <44743368+Aben-Bel@users.noreply.github.com>
2023-04-20 07:40:12 +0300

    Login signout flow (#51)
    * [Fronted] implement landing page with navbar, hero, features and footer components
    
    * [Frontend] fix sign in and sign out and add loading bar for application, home page

--- frontend/src/  components/MyGigCard.js
+++ frontend/src/  components/MyGigCard.js
@@ -6,16 +6,16 @@ import { useNavigate } from "react-router-dom";
 export const MyGigCard = ({ gig }) => {
     const navigate = useNavigate();
     return (
-        <div className="card w-full bg-base-100 hover:shadow-md" onClick={() => navigate(`/gigs/${gig._id}`)}>
+        <div className="w-full card bg-base-100 hover:shadow-md" onClick={() => navigate(`/gigs/${gig._id}`)}>
             <div className="card-body">
-                <h2 className="card-title font-bold text-neutral">{gig.title}</h2>
+                <h2 className="font-bold card-title text-neutral">{gig.title}</h2>
                 <p className="font-regular text-neutral-400">{gig.description}</p>
-                <div className="flex justify-between items-center pt-4">
-                    <div className="flex gap-2 text-neutral-400 items-center">
+                <div className="flex items-center justify-between pt-4">
+                    <div className="flex items-center gap-2 text-neutral-400">
                         <Clipboard size={20} />
                         <span>{gig.applications.length}</span>
                     </div>
-                    <div className="flex gap-2 text-neutral-400 items-center">
+                    <div className="flex items-center gap-2 text-neutral-400">
                         <div>
                             Posted <ReactTimeAgo date={gig.createdAt} locale="en-US" />
                         </div>
