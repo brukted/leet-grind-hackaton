6a64739bdd94568d1abbbbd741ff7757313c6c82
Abenezer <44743368+Aben-Bel@users.noreply.github.com>
2023-04-20 09:04:29 +0300

    Fix authorized paths (#53)
    * [Fronted] implement landing page with navbar, hero, features and footer components
    
    * [Frontend] fix authorized paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { RecoilRoot } from "recoil";
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { SignIn } from "./pages/SignInPage/SignInPage";
 import { SignUp } from "./pages/SignUpPage/SignUpPage";
 import LandingPage from "./pages/LandingPage/LandingPage";
@@ -8,50 +8,73 @@ import ApplicationApply from "./pages/ApplicationApplyForm.js/ApplicationApplyFo
 import ApplicationDetailPage from "./pages/ApplicationDetailPage/ApplicationDetailPage";
 import { GigDetailPage } from "./pages/GigDetailPage/GigDetailPage";
 import { ApplicantInfoPage } from "./pages/ApplicantInfoPage/ApplicantInfoPage";
+import { RecoilRoot } from "recoil";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LandingPage />,
-  },
-  {
-    path: "/apply/:id",
-    element: <ApplicationApply />,
-  },
-  {
-    path: "/applications/:id",
-    element: <ApplicationDetailPage />,
-  },
-  {
-    path: "/applicantInfo/:applicationId",
-    element: <ApplicantInfoPage />,
-  },
-  {
-    path: "/gigs/:id",
-    element: <GigDetailPage />,
-  },
-  {
-    path: "/home",
-    element: <HomePage />,
-  },
-  {
-    path: "/sign-in",
-    element: <SignIn />,
-  },
-  {
-    path: "/",
-    element: <LandingPage />,
-  },
-  {
-    path: "/sign-up",
-    element: <SignUp />,
-  },
-]);
+const isAuthenticated = () => {
+  console.log("is logged in: ", localStorage.getItem("authToken") !== null);
+  return localStorage.getItem("authToken") !== null;
+};
 
 function App() {
   return (
     <RecoilRoot>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route
+            path="/apply/:id"
+            element={
+              isAuthenticated() ? (
+                <ApplicationApply />
+              ) : (
+                <Navigate to="/" replace={true} />
+              )
+            }
+          />
+          <Route
+            path="/applications/:id"
+            element={
+              isAuthenticated() ? (
+                <ApplicationDetailPage />
+              ) : (
+                <Navigate to="/" replace={true} />
+              )
+            }
+          />
+          <Route
+            path="/applicantInfo/:applicationId"
+            element={
+              isAuthenticated() ? (
+                <ApplicantInfoPage />
+              ) : (
+                <Navigate to="/" replace={true} />
+              )
+            }
+          />
+          <Route
+            path="/gigs/:id"
+            element={
+              isAuthenticated() ? (
+                <GigDetailPage />
+              ) : (
+                <Navigate to="/sign-in" replace={true} />
+              )
+            }
+          />
+
+          <Route
+            path="/home"
+            element={isAuthenticated() ? <HomePage /> : <Navigate to="/sign-in" />}
+          />
+
+          <Route
+            path="*"
+            element={isAuthenticated() ? <HomePage /> : <Navigate to="/sign-in" />}
+          />
+        </Routes>
+      </BrowserRouter>
     </RecoilRoot>
   );
 }
