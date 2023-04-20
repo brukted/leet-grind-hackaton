3ba214e307dab17f182c30e2c748ab1220f34fcf
aben-bel <abenezer.sbelay@gmail.com>
2023-04-20 07:39:40 +0300

    [Frontend] fix sign in and sign out and add loading bar for application, home page

--- frontend/src/pages/HomePage/TabPages/Home.js
+++ frontend/src/pages/HomePage/TabPages/Home.js
@@ -9,10 +9,10 @@ const Home = () => {
   const [ideas, setIdeas] = useRecoilState(ideasState);
   const [page, setPage] = useState(1);
   const [pageSize, setPageSize] = useState(6);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     getGigs().then((response) => {
-      console.log("Response: ", response);
       const tags = new Set();
       const results = response.map((val) => {
         val.tags.forEach((element) => {
@@ -28,6 +28,7 @@ const Home = () => {
           githubLink: val.ideaModel.github,
         };
       });
+      setLoading(false);
       setIdeas(results || []);
       setAvailableTags(Array.from(tags));
     });
@@ -115,74 +116,100 @@ const Home = () => {
           ))}
         </div>
       </div>
-
-      <div class="flex justify-center items-center flex-col md:flex-row space-y-4 md:space-x-4">
-        <div class="flex justify-center items-center space-x-2">
-          <div class="flex items-center">
-            <span class="mr-2">Page</span>
-            <input
-              type="number"
-              value={page}
-              onChange={(e) => setPage(Number(e.target.value))}
-              min="1"
-              max={totalPages}
-              class="w-16 p-2 border-2 border-gray-200 rounded-lg"
+      {isLoading ? (
+        <div role="status" class="flex justify-center items-center space-x-2">
+          <svg
+            aria-hidden="true"
+            class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
             />
-            <span class="ml-2">of {totalPages}</span>
-          </div>
-          <button
-            class="ml-4 px-4 py-2 text-white bg-primary rounded-lg shadow-lg hover:bg-primary-dark transition-colors duration-300"
-            onClick={() => setPage(page - 1)}
-            disabled={page === 1}
-          >
-            Prev
-          </button>
-          <button
-            class="ml-2 px-4 py-2 text-white bg-primary rounded-lg shadow-lg hover:bg-primary-dark transition-colors duration-300"
-            onClick={() => setPage(page + 1)}
-            disabled={page === totalPages}
-          >
-            Next
-          </button>
-        </div>
-      </div>
-
-      <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {visibleIdeas.map((idea) => (
-          <IdeaCard key={idea.id} idea={idea} />
-        ))}
-      </div>
-
-      <div class="flex justify-center items-center flex-col md:flex-row space-y-4 md:space-x-4">
-        <div class="flex justify-center items-center space-x-2">
-          <div class="flex items-center">
-            <span class="mr-2">Page</span>
-            <input
-              type="number"
-              value={page}
-              onChange={(e) => setPage(Number(e.target.value))}
-              min="1"
-              max={totalPages}
-              class="w-16 p-2 border-2 border-gray-200 rounded-lg"
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
             />
-            <span class="ml-2">of {totalPages}</span>
-          </div>
-          <button
-            class="ml-4 px-4 py-2 text-white bg-primary rounded-lg shadow-lg hover:bg-primary-dark transition-colors duration-300"
-            onClick={() => setPage(page - 1)}
-            disabled={page === 1}
-          >
-            Prev
-          </button>
-          <button
-            class="ml-2 px-4 py-2 text-white bg-primary rounded-lg shadow-lg hover:bg-primary-dark transition-colors duration-300"
-            onClick={() => setPage(page + 1)}
-            disabled={page === totalPages}
-          >
-            Next
-          </button>
+          </svg>
+          <span class="sr-only">Loading...</span>
         </div>
-      </div>
+      ) : (
+        <div>
+          <div class="flex justify-center items-center flex-col md:flex-row space-y-4 md:space-x-4">
+            <div class="flex justify-center items-center space-x-2">
+              <div class="flex items-center">
+                <span class="mr-2">Page</span>
+                <input
+                  type="number"
+                  value={page}
+                  onChange={(e) => setPage(Number(e.target.value))}
+                  min="1"
+                  max={totalPages}
+                  class="w-16 p-2 border-2 border-gray-200 rounded-lg"
+                />
+                <span class="ml-2">of {totalPages}</span>
+              </div>
+
+              <button
+                class="ml-4 px-4 py-2 text-white bg-primary rounded-lg shadow-lg hover:bg-primary-dark transition-colors duration-300"
+                onClick={() => setPage(page - 1)}
+                disabled={page === 1}
+              >
+                Prev
+              </button>
+              <button
+                class="ml-2 px-4 py-2 text-white bg-primary rounded-lg shadow-lg hover:bg-primary-dark transition-colors duration-300"
+                onClick={() => setPage(page + 1)}
+                disabled={page === totalPages}
+              >
+                Next
+              </button>
+            </div>
+          </div>
+
+          <br></br>
+          <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+            {visibleIdeas.map((idea) => (
+              <IdeaCard key={idea.id} idea={idea} />
+            ))}
+          </div>
+
+          <br></br>
+          <div class="flex justify-center items-center flex-col md:flex-row space-y-4 md:space-x-4">
+            <div class="flex justify-center items-center space-x-2">
+              <div class="flex items-center">
+                <span class="mr-2">Page</span>
+                <input
+                  type="number"
+                  value={page}
+                  onChange={(e) => setPage(Number(e.target.value))}
+                  min="1"
+                  max={totalPages}
+                  class="w-16 p-2 border-2 border-gray-200 rounded-lg"
+                />
+                <span class="ml-2">of {totalPages}</span>
+              </div>
+              <button
+                class="ml-4 px-4 py-2 text-white bg-primary rounded-lg shadow-lg hover:bg-primary-dark transition-colors duration-300"
+                onClick={() => setPage(page - 1)}
+                disabled={page === 1}
+              >
+                Prev
+              </button>
+              <button
+                class="ml-2 px-4 py-2 text-white bg-primary rounded-lg shadow-lg hover:bg-primary-dark transition-colors duration-300"
+                onClick={() => setPage(page + 1)}
+                disabled={page === totalPages}
+              >
+                Next
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
