2da74f4b5f8429aece27c014f50bbdcf459fc6ec
Bruk Tedla <biruk.tedla93@gmail.com>
2023-04-20 11:08:57 +0300

    implement my ideas tab

--- frontend/src/pages/ApplicationApplyForm.js/ApplicationApplyForm.js
+++ frontend/src/pages/ApplicationApplyForm.js/ApplicationApplyForm.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { ideasState } from "../../recoil_state";
-import { useNavigate, useParams } from "react-router-dom";
-import IdeaCard from "../HomePage/components/IdeaCard";
 import { createApplication } from "../../services/applicationService";
 
 const ApplicationApply = () => {
